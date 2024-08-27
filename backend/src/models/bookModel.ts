dfe7c0d4205150299a0adf27ca2c9f15e5a60577
bluebaney10 <bluebaney10@gmail.com>
2024-08-27 21:30:08 +0700

    Create Book model and start sample get all items

---
+++ backend/src/models/bookModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IBook extends Document {
+  title: string;
+  author: string;
+  publishYear: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const bookSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model<IBook>("Book", bookSchema);
+
+export default Book;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from "./db";
+import Book from "./models/bookModel";
 
 const app = express();
 const PORT = 3333;
 
-// Connect to MongoDB
 connectDB();
 
 app.get("/", (request, response) => {
@@ -13,21 +13,17 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome to Tutorial");
 });
 
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 app.listen(PORT, () => console.log(`Server is started at port ${PORT}`));
-
-declare var process: {
-  env: {
-    mongoDBURL: string;
-  };
-};
-
-/* console.log("process.env.mongoDBURL:", process.env.mongoDBURL);
-mongoose
-  .connect(process.env.mongoDBURL)
-  .then(() => {
-    console.log("App connected to database");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
- */
