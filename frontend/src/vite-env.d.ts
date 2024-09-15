70f1f395fa97c356e680c6574d9a062580dc1387
bluebaney10 <bluebaney10@gmail.com>
2024-09-16 00:33:10 +0700

    fix env variable

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 export interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
-  readonly AUTH0_DOMAIN: string;
-  readonly AUTH0_CLIENT_ID: string;
+  readonly VITE_AUTH0_DOMAIN: string;
+  readonly VITE_AUTH0_CLIENT_ID: string;
 }
 
 interface ImportMeta {
