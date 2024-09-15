6698ca22cd5b33a8f62eda5397aa5ed7b31c70df
bluebaney10 <bluebaney10@gmail.com>
2024-09-15 23:33:45 +0700

    add disable react devtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,12 @@ import "./styles/index.css";
 import "./styles/button.css";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 const domain: string = import.meta.env.VITE_AUTH0_DOMAIN ?? "";
 const clientId: string = import.meta.env.VITE_AUTH0_CLIENT_ID ?? "";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
